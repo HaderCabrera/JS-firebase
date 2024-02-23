8e23122df6074bdb3350708dcefcaf9c7bc8d094
HaderCabrera <hader.cabrera96@gmail.com>
2024-02-23 15:30:53 -0500

    corregir refresco de pagina

---
+++ firebase.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+  import { } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyASGes0_m2X4xxkg0jFfUxK0ZrGVrhrePw",
+    authDomain: "fir-js-4c432.firebaseapp.com",
+    projectId: "fir-js-4c432",
+    storageBucket: "fir-js-4c432.appspot.com",
+    messagingSenderId: "856936629524",
+    appId: "1:856936629524:web:62648b4f2399da70c7ae01"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+})
+
+const taskForm = document.getElementById('task-form')
+taskForm.addEventListener('onclick', (e) => {
+    e.preventDefault;
+    console.log("estoy enviando algo")
+
+})
